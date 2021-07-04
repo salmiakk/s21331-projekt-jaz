e2c099833864586077dae03ebbd9b7ab520fd6eb
Mateusz Pioch <s21331@pjwstk.edu.pl>
2021-07-04 14:07:23 +0200

    init

---
+++ src/main/java/com/pjatk/jazs21331nbp/JazS21331NbpApplication.java
@@ -0,0 +1,13 @@
+package com.pjatk.jazs21331nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS21331NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS21331NbpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pjatk/jazs21331nbp/JazS21331NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pjatk.jazs21331nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS21331NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
